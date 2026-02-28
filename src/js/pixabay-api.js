f7822e76d5dc345a6e5b513fc3d1ba5a9a411cc9
Artem Chupylko <achupylko@gmail.com>
2026-02-28 14:58:05 +0000

    fix api key

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,12 +1,10 @@
 import axios from 'axios';
 
-const API_KEY = '54679723-82f3546e98f06279d87186197';
-
 function getImagesByQuery(query) {
   return axios
     .get('https://pixabay.com/api/', {
       params: {
-        key: API_KEY,
+        key: import.meta.env.VITE_API_KEY,
         q: query,
         image_type: 'photo',
         orientation: 'horizontal',
